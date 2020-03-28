39d5d5ab315e14f52706851032d27400ef141079
Tom Young <hello@tomyoung.co.nz>
2020-03-29 11:13:23 +1300

    Only use CNAME record

--- infra/tf/main.tf
+++ infra/tf/main.tf
@@ -3,22 +3,10 @@ provider "aws" {
   region  = "us-east-1"
 }
  
-resource "aws_route53_record" "root" {
-    zone_id = var.coremoter_zone_id
-    name = "coremoter.com"
-    type = "A"
-    ttl = "300"
-    records = [ 
-        "185.199.108.153",
-        "185.199.109.153",
-        "185.199.110.153",
-        "185.199.111.153"]
-}
-
 resource "aws_route53_record" "www" {
     zone_id = var.coremoter_zone_id
     name = "www.coremoter.com"
-    type = "A"
+    type = "CNAME"
     ttl = "300"
-    records = aws_route53_record.root.records
+    records = "fiq.github.io"
 }
